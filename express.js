6bf6642189f82185dc04194a1e4dbd9c4eeaa872
Anku kumar <ankukumar@Ankus-MacBook-Air.local>
2026-04-03 17:28:57 +0530

    restore db connection import

--- express.js
+++ express.js
@@ -1,5 +1,5 @@
 import express from "express";
-//import db from "./db.js";
+import db from "./db.js";
 import Person from "./models/Person.js";
 import MenuItem from "./models/MenuItem.js";
 import bodyParser from "body-parser";
